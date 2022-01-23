b89679fa43a75055ffa74fe8f5e05eed709a6e56
Michelle May <michelle@michellemay.co.uk>
2022-01-23 14:43:35 +0000

    njk to html. add header

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,21 @@
-module.exports = function (eleventyConfig) {
+module.exports = eleventyConfig => {
+
+  // Layout aliases
+  eleventyConfig.addLayoutAlias('base', 'layouts/base.html')
+
+     // Include our static assets
+     eleventyConfig.addPassthroughCopy('./src/images')
+ 
+
   return {
+    templateFormats: ["md", "njk"],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+
     dir: {
       input: "src",
       output: "public",
     },
-  };
-};
+  }
+}
