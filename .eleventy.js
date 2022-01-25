543119139c6f9eea6427157028c8dd09b5ef6ace
Michelle May <michelle@michellemay.co.uk>
2022-01-25 22:30:37 +0000

    Update naming, add some data

--- .eleventy.js
+++ .eleventy.js
@@ -3,19 +3,18 @@ module.exports = eleventyConfig => {
   // Layout aliases
   eleventyConfig.addLayoutAlias('base', 'layouts/base.html')
 
-     // Include our static assets
-     eleventyConfig.addPassthroughCopy('./src/images')
- 
+  // Include our static assets
+  // eleventyConfig.addPassthroughCopy('./src/images')
+
 
   return {
-    templateFormats: ["md", "njk"],
     markdownTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     passthroughFileCopy: true,
 
     dir: {
       input: "src",
-      output: "public",
-    },
+      output: "public"
+    }
   }
 }
