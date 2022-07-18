83a8ec1e84657fc99a088f5a5da16f8aa2e3e586
Michelle May <michellem4y@gmail.com>
2022-07-18 18:11:20 +0100

    Add fade in

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,20 +1,20 @@
 // Navigation
 
-const navToggle = document.querySelector(".nav-toggle")
-const nav = document.querySelector(".main-nav")
-const navClose = document.querySelector(".main-nav__close")
+// const navToggle = document.querySelector(".nav-toggle")
+// const nav = document.querySelector(".main-nav")
+// const navClose = document.querySelector(".main-nav__close")
 
 
 // If the toggle is clicked, toggle the mobile-menu-open class in the html element
-navToggle.addEventListener("click", function () {
-    nav.classList.add("nav-active")
-    navToggle.setAttribute("aria-expanded", "true")
-})
+// navToggle.addEventListener("click", function () {
+//     nav.classList.add("nav-active")
+//     navToggle.setAttribute("aria-expanded", "true")
+// })
 
-navClose.addEventListener("click", function() {
-    nav.classList.remove("nav-active")
-    navToggle.setAttribute("aria-expanded", "false")
-})
+// navClose.addEventListener("click", function() {
+//     nav.classList.remove("nav-active")
+//     navToggle.setAttribute("aria-expanded", "false")
+// })
 
 
 // Smooth scroll links
@@ -36,41 +36,41 @@ scrollLinks.forEach(link => {
   })
 })
 
-// const animatedTags = document.querySelectorAll(".js-fade")
+const animatedTags = document.querySelectorAll(".js-fade")
+console.log(animatedTags)
 
-// // fade out on load
-// animatedTags.forEach(tag => {
-//   tag.style.opacity = 0
-// })
+// fade out on load
+animatedTags.forEach(tag => {
+  tag.style.opacity = 0
+})
 
-// const fadeIn = function () {
-//   // look through all the animated tags and see
-//   // with the getBoundingClientRect if it's in the window
-//   let delay = 0.25  
+const fadeIn = function () {
+  // look through all the animated tags and see
+  // with the getBoundingClientRect if it's in the window
+  let delay = 0.25  
   
-//   animatedTags.forEach(tag => {
-//     const tagTop = tag.getBoundingClientRect().top
-//     const tagBottom = tag.getBoundingClientRect().bottom
+  animatedTags.forEach(tag => {
+    const tagTop = tag.getBoundingClientRect().top
+    const tagBottom = tag.getBoundingClientRect().bottom
+
+    console.log(tagTop)
     
-//     if (tagTop < window.innerHeight && tagBottom > 0) {
-//       tag.style.animation = `fadein 1s ${delay}s both`
-//       delay = delay + 0.25
-//     } else {
-//       tag.style.opacity = 0
-//       tag.style.animation = ""
-//     }
-//   })
-// }
+    if (tagTop < window.innerHeight && tagBottom > 0) {
+      tag.style.animation = `fadein 1s ${delay}s forwards`
+      delay = delay + 0.25
+    } 
+  })
+}
 
-// // on load, run fadeIn
-// fadeIn()
+// on load, run fadeIn
+fadeIn()
 
-// // on scroll, run fadeIn
-// document.addEventListener("scroll", function () {
-//   fadeIn()
-// })
+// on scroll, run fadeIn
+document.addEventListener("scroll", function () {
+  fadeIn()
+})
 
-// // on browser resize, run fadeIn
-// window.addEventListener("resize", function () {
-//   fadeIn()
-// })
+// on browser resize, run fadeIn
+window.addEventListener("resize", function () {
+  fadeIn()
+})
